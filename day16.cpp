b00cbb6277515e22f0b6269e4a2f6840ff9b0f5e
CBadlani <146823529+CBadlani@users.noreply.github.com>
2024-09-24 21:33:06 +0530

    Add files via upload

---
+++ day16.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int N, int M) {
+    return (N * M) / gcd(N, M); 
+}
+
+int main() {
+    int N = 4, M = 6;  
+    cout << "LCM of " << N << " and " << M << " is: " << lcm(N, M) << endl;
+
+    return 0;
+}
